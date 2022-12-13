4d90922fa1eb1bcf4220a54059082b33366eed6f
Shruti Bansod <shruti.bansod@vritti.co.in>
2022-12-13 12:54:58 +0530

    adding department component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DepartmentComponent } from './department/department.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'app', loadChildren: () => import('./app.module').then(m => m.AppModule) },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/department/department.component.ts
+++ src/app/department/department.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Console } from 'console';
 
 @Component({
   selector: 'app-department',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./department.component.scss']
 })
 export class DepartmentComponent implements OnInit {
-  
+  code:string="";
+  Description:string="";
   constructor() { }
-
   ngOnInit(): void {
   }
-
+  ReadData(){
+    this.code = (<HTMLInputElement>document.getElementById("code")).value;
+    this.Description = (<HTMLInputElement>document.getElementById("Description")).value;
+    console.log("Code:"+this.code);
+    console.log("Description:"+this.Description);
+  }
 }
